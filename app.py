383dd7cc6943f3bf7505b4a4e5f8c18b4946e1f2
Anita  Lamai <anitalamaii27@gmail.com>
2025-12-28 02:14:18 -0500

    Bind Flask app to 0.0.0.0 and use PORT env variable for Render

--- app.py
+++ app.py
@@ -323,6 +323,8 @@ def import_csv():
 # -------------------------------
 # Run Flask app
 # -------------------------------
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render provides the PORT env variable
+    app.run(host="0.0.0.0", port=port, debug=True)
 
